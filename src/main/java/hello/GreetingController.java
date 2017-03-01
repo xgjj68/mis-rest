0528ff994f55b1bca46ba01b65dfc80a3eace9e5
rouqinglengjian <zhangpeng@dayang.com.cn>
2017-03-01 11:09:37 +0800

    测试git用户

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -13,6 +13,7 @@ public class GreetingController {
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+    	System.out.println("测试git用户");
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
     }
